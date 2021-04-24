0d0097f3b22e5946802f872555a58562fcec2eab
akiyoshi-takazawa <69133113+akiyoshi-takazwa@users.noreply.github.com>
2021-04-24 19:43:47 +0900

    learn props-type

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
 
-function App() {
+const App = () => {
+  const profiles = [
+    { name: "月田", age: 1},
+    { name: "泰明", age: 99},
+    { name: "高澤"}
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello
-        </p>
-      </header>
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
-  );
+  )
+}
+
+const User = (props) => {
+return <div>Hi, I am {props.name}, and {props.age} years old!!!!</div>
+}
+
+User.defaultProps = {
+  age: 1000000
+}
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
