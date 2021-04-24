daade2f4f5c82a44e2541904cc8d9fa0779ec3fc
akiyoshi-takazawa <69133113+akiyoshi-takazwa@users.noreply.github.com>
2021-04-24 13:15:23 +0900

    fix leran JSX

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello
-        </p>
+          <h1>Hello</h1>
       </header>
     </div>
   );
 }
+
+// 上記の書き方がJSXとなる
+// react内で下記のように変換される
+// JSXの利点は直感的に書きやすい。
+
+// function App() {
+//   return React.createElement(
+//     "div",
+//     null,
+//     "hello world",
+//   );
+// }
 
 export default App;
