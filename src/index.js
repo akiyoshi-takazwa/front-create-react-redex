a7581859177ed4c2631f1e6d6ff2518b83e83fb8
akiyoshi-takazawa <69133113+akiyoshi-takazwa@users.noreply.github.com>
2021-04-25 08:10:40 +0900

    learn store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
+import reducer from './reducers';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
