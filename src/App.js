00285e2305b991a1dcc2c0df76ec62bdcdb09148
akiyoshi-takazawa <69133113+akiyoshi-takazwa@users.noreply.github.com>
2021-04-24 18:52:00 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import logo from './logo.svg';
 
-function App() {
+const App = () =>{
+  const profiles = [
+    { name: "taro", age: 10},
+    { name: "akiyoshi", age: 999999},
+    { name: "NoName"}
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello
-        </p>
-      </header>
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
     </div>
-  );
+  )
+}
+
+const User = (props) => {
+return <div>Hi, I am {props.name}!!, and {props.age} years old.</div>
+}
+
+User.defaultProps = {
+  age: "('ω')"
 }
 
 export default App;
